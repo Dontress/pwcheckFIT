f5f946a5b0bc1775eb921b70a1098cfc400a6254
Petr Šíma <45285946+Dontress@users.noreply.github.com>
2021-10-17 13:37:47 +0200

    InputOutput
    Osetrena maximalni delka, odradkovani hesel ve vsech pripadech a funguje vstup a vystup s filtrem na maximalni delku

--- pwcheck.c
+++ pwcheck.c
@@ -2,18 +2,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void stdinToString();
-bool containsChar(char c);
-void stringToStdout();
+#define MAXLEN 100
 
-char passwdString[100];
+void stdinToString();
+void stringToStdout();
+bool containsLower();
+bool containsUpper();
+bool containsNumber();
+bool containsSpecialChar();
+
+char passwdString[MAXLEN];
 bool endOfFile = false;
+bool isNormalSize;
 
 int main(int argc, char *argv[]){
 
     do{
         stdinToString();
-        stringToStdout();
+
+        if( isNormalSize )
+            stringToStdout();
 
     }while( endOfFile == false );                       
  
@@ -24,27 +32,90 @@ return 0;
 void stdinToString(){
     char c;
     int i;
+
+    isNormalSize = true;
     
     for (i = 0; (c = getchar()) != '\n'; i++)
     {
-        if(c == EOF){
+         if(c == EOF){
             endOfFile = true;
             break;
         }
+
+        if(i >= MAXLEN){            // osetreni presahnuti maximalni mozne delky => heslo je povazovano za neplatne
+            isNormalSize = false;
+            continue;
+        }
+
         passwdString[i] = c;
     }
-    
-    passwdString[i] = '\0';                              
+
+    if(isNormalSize)
+        passwdString[i] = '\0';                        
 }
 
 // posila prave nactene heslo (globalni string) do stdout
 void stringToStdout(){
 
+    static int passwdNumber = 0; //zajistuje ze prvni heslo nebude odradkovane
+
+    if( passwdNumber > 0) // zajistuje odradkovani hesel na vystupu
+        putchar('\n');
+
     for (int i = 0; passwdString[i] != '\0' ; i++)
         putchar( passwdString[i] );
+
+    passwdNumber++;
+}
+
+// vraci, jestli heslo obsahuje alespon jedno male pismeno
+bool containsLower(){
+
+    for (int i = 0; passwdString[i] != '\0'; i++)
+    {
+        if(passwdString[i] >= 'a' && passwdString[i] <= 'z')
+            return true;
+    }
     
-    if(endOfFile != true)
-        putchar('\n');
+return false;
+}
+
+// vraci, jestli heslo obsahuje alespon jedno velke pismeno
+bool containsUpper(){
+
+    for (int i = 0; passwdString[i] != '\0'; i++)
+    {
+        if(passwdString[i] >= 'A' && passwdString[i] <= 'Z')
+            return true;
+    }
+    
+return false;
+}
+
+// vraci, jestli heslo obsahuje alespon jedno cislo
+bool containsNumber(){
+    for (int i = 0; passwdString[i] != '\0'; i++)
+    {
+        if(passwdString[i] >= '0' && passwdString[i] <= '9')
+            return true;
+    }
+    
+return false;
+}
+
+// vraci, jestli heslo obsahuje alespon jeden specialni znak
+bool containsSpecialChar(){
+
+    for (int i = 0; passwdString[i] != '\0'; i++)
+    {
+        if( passwdString[i] >= 32 && passwdString[i] < '0'  || 
+            passwdString[i] > '9' && passwdString[i] < 'A'  ||
+            passwdString[i] > 'Z' && passwdString[i] < 'a'  ||
+            passwdString[i] > 'z' && passwdString[i] <= 126    )
+                return true;
+    }
+    
+return false;
 }
 
 
